c85fd09c7a9e8750218bf9fdacdf68c1a73399ab
marahmanjs <imarahmandev@gmail.com>
2023-02-04 17:00:13 +0700

    recover component

---
+++ src/routes/[area]/recover/+page.svelte
@@ -0,0 +1,9 @@
+
+<script lang="ts">
+  import { page } from '$app/stores';
+	import Reset from '../../../components/auth/reset.svelte';
+
+  let area = $page.params.area
+</script>
+
+<Reset area={area}/>
---
+++ src/routes/recover/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import Reset from "../../components/auth/reset.svelte";
+</script>
+
+<Reset />
